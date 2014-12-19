b4b190c8df9398fd6d4df12d289b7251409ca960
Anderson Silva <andsf@live.com>
2014-12-19 09:46:37 -0300

    Recuperação de Senha - Em construção

--- src/Util/VeriListener.java
+++ src/Util/VeriListener.java
@@ -9,44 +9,46 @@ import javax.faces.event.PhaseListener;
 
 import Beans.PessoasBean;
 
+public class VeriListener implements PhaseListener {
 
-public class VeriListener implements PhaseListener{
-
-	
 	public void afterPhase(PhaseEvent event) {
 		FacesContext context = event.getFacesContext().getCurrentInstance();
 		FacesContext con = FacesContext.getCurrentInstance();
-		 if("/index.xhtml".equals(context.getViewRoot().getViewId()) || 
-			"/Registros.xhtml".equals(context.getViewRoot().getViewId()) ||
-			"/VisualizarLivros.xhtml".equals(context.getViewRoot().getViewId()) ||
-			"/HelpScreenSenha.xhtml".equals(context.getViewRoot().getViewId()) ||
-			"/VisualizarUsuarios.xhtml".equals(context.getViewRoot().getViewId())){
-			 
-			 return;
-		 }
-		 
-			PessoasBean bean = (PessoasBean) context.getApplication().evaluateExpressionGet(context, "#{bPessoa}", PessoasBean.class) ;
-		 
-		 
-		 if(!bean.logado()){
-			 NavigationHandler nv = context.getApplication().getNavigationHandler();
-			 nv.handleNavigation(context, null, "index?faces-redirect=true");
-			 con.addMessage(null, new FacesMessage("Erro!" , "É nececessário estar logado para acessar essa página") );
-			 System.out.println("Processamento Incorreto: Favor faça Login");
-			 context.renderResponse();
-			 
-		 }
-		 
+		if ("/index.xhtml".equals(context.getViewRoot().getViewId())
+				|| "/Registros.xhtml".equals(context.getViewRoot().getViewId())
+				|| "/VisualizarLivros.xhtml".equals(context.getViewRoot()
+						.getViewId())
+				|| "/HelpScreenSenha.xhtml".equals(context.getViewRoot()
+						.getViewId())
+				|| "/VisualizarUsuarios.xhtml".equals(context.getViewRoot()
+						.getViewId())) {
+
+			return;
+		}
+
+		PessoasBean bean = (PessoasBean) context
+				.getApplication()
+				.evaluateExpressionGet(context, "#{bPessoa}", PessoasBean.class);
+
+		if (!bean.logado()) {
+			NavigationHandler nv = context.getApplication()
+					.getNavigationHandler();
+			nv.handleNavigation(context, null, "index?faces-redirect=true");
+			con.addMessage(null, new FacesMessage("Erro!",
+					"É nececessário estar logado para acessar essa página"));
+			System.out.println("Processamento Incorreto: Favor faça Login");
+			context.renderResponse();
+
+		} 
 	}
 
 	public void beforePhase(PhaseEvent event) {
-		
+
 	}
 
 	public PhaseId getPhaseId() {
-		
+
 		return PhaseId.RESTORE_VIEW;
 	}
-	
-	
+
 }
